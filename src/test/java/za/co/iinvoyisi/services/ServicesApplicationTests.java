7bd138646a9614349d4d0634f67786cec33b2c7d
Xxtractz <musambaloyi@gmail.com>
2021-07-09 22:42:17 +0200

    #comment initial Spring boot project

---
+++ src/test/java/za/co/iinvoyisi/services/ServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.iinvoyisi.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
